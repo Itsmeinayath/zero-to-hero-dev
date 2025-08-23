f6e8767098a837509232af446683e97cdcf3ccc0
Mohammed Inayath <mohammedinayath1258@gmail.com>
2025-08-23 23:56:18 +0530

    Add initial implementation of functions: sum, greet, and square; include example usage

---
+++ Logic building/Day-04/function.py
@@ -0,0 +1,21 @@
+# Sum of two values
+# def sum_two_values(a,b):
+#     return a + b
+
+# # Example usage
+# print(sum_two_values(int(input("Enter first value: ")), int(input("Enter second value: "))))  # Input: 10, 20
+# print(sum_two_values(10, 20))  # Output: 30
+
+# Function to greet a user
+
+# def greet(name):
+#     return f"Hello,{name}!"
+
+# print(greet("Inyath"))
+
+# Funtion to print square
+
+def square(num):
+    return num * num
+
+print(square(10))
