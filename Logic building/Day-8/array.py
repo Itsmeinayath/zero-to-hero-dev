c668b44e0270ae5b57b3f575dfb00941c7494d8e
Mohammed Inayath <mohammedinayath1258@gmail.com>
2025-08-25 13:37:59 +0530

    Add Day 8 content: Arrays theory, practice problems, and reflection

---
+++ Logic building/Day-8/array.py
@@ -0,0 +1,6 @@
+# What is an Array?
+# An array is a data structure that stores a collection of elements, typically of the same data type.
+# In Python, arrays are implemented as lists, which are dynamic and can hold elements of different types.
+# Lists in Python are versatile and can be used to implement various data structures, such as stacks, queues, and more.
+
+# 
