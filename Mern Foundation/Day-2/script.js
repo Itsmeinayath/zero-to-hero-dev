9b89c1d98872f36ca38964ad3dcc74ec4c3521bc
Mohammed Inayath <mohammedinayath1258@gmail.com>
2025-09-19 12:04:41 +0530

    Add comprehensive learning materials and checkpoint assessment for Week 4
    - Created README for Day 26 focusing on mixed practice problems including Maximum Subarray and Kth Largest Element.
    - Developed README for Day 27 detailing a checkpoint assessment covering key problems from Weeks 1-3.
    - Introduced a review structure for Day 28 to reflect on Week 4 learnings and encourage rest.

---
+++ Mern Foundation/Day-2/script.js
@@ -0,0 +1,16 @@
+// old javascript
+// var counter = 6
+// console.log("Counter is:", counter);
+// counter = 8
+// console.log("Counter is now:", counter);
+// var counter = 10
+// console.log("Counter is finally:", counter);
+
+// new javascript
+// let counter = 6
+// console.log("Counter is:", counter);
+// counter = 8
+// console.log("Counter is now:", counter);
+// let counter = 10
+// console.log("Counter is finally:", counter);
+
